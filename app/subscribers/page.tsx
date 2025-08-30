3351812bde2b8cab8a592a7233c1de2fb720108c
MelakuGirma13 <MelakuGirma13@gmail.com>
2025-08-30 19:32:39 +0300

    disable-catch-subscribers-page

--- app/subscribers/page.tsx
+++ app/subscribers/page.tsx
@@ -1,15 +1,23 @@
+'use server'
+
 import { getSubscribersAction } from "@/lib/actions";
 import React from "react";
 
+// Disable caching entirely
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 async function Subscribers() {
   const res = await getSubscribersAction();
-  console.log(res);
+  console.log("Fetched subscribers:", res);
   return (
     <>
       <h1>Subscribers List ({res.data?.length})</h1><br/>
       <ul>
-        {res.data?.map((subscriber,index) => (
-          <li key={subscriber.id}>{index + 1} - {subscriber.email} = {subscriber.createdAt.toDateString()}</li>
+        {res.data?.map((subscriber, index) => (
+          <li key={subscriber.id}>
+            {index + 1} - {subscriber.email} = {subscriber.createdAt.toDateString()}
+          </li>
         ))}
       </ul>
     </>
